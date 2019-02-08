edc106dac08d4b737675f7a897d77c89b7de01a9
weto <weto.jc@gmail.com>
2019-02-08 00:26:24 +0000

    Alterando package

--- src/main/java/com/javaee/email/EmailConfig.java
+++ src/main/java/com/javaee/email/EmailConfig.java
@@ -1,3 +1,4 @@
+package com.javaee.email;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
--- src/main/java/com/javaee/email/EmailSender.java
+++ src/main/java/com/javaee/email/EmailSender.java
@@ -1,3 +1,4 @@
+package com.javaee.email;
 
 public class EmailSender {
 
@@ -5,8 +6,8 @@ public class EmailSender {
 		// TODO Auto-generated method stub
 
 		final String fromEmail = "137483ada09988";
-		final String password = "5f4bce8e72b79";
-		final String toEmail = "weto.jc@gmail.com";
+		final String password = "5f4bceb8e72b79";
+		final String toEmail = "teste@gmail.com";
 		
 		System.out.println("Iniciando");
 		EmailConfig config = new EmailConfig();
